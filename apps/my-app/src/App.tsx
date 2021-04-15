d72a2ed9bfd4e701bd53fb8468498a71be55445e
joshamos <josh.amos@99designs.com>
2021-04-15 22:37:47 +1000

    Fix demo

--- apps/my-app/src/App.tsx
+++ apps/my-app/src/App.tsx
@@ -1,21 +1,21 @@
-// import * as React from 'react';
-// import { MyLib2 } from '@vite-tsconfig-paths-demo/my-lib2';
-//
-// export const App = () => (
-//   <>
-//     <MyLib2 />
-//   </>
-// );
-
-// Comment above, and uncomment below to see it working if both are used
-
 import * as React from 'react';
-import { MyLib1 } from '@vite-tsconfig-paths-demo/my-lib1';
 import { MyLib2 } from '@vite-tsconfig-paths-demo/my-lib2';
 
 export const App = () => (
   <>
-    <MyLib1 />
     <MyLib2 />
   </>
 );
+
+// Comment out above, and uncomment below to see it working if both are used
+//
+// import * as React from 'react';
+// import { MyLib1 } from '@vite-tsconfig-paths-demo/my-lib1';
+// import { MyLib2 } from '@vite-tsconfig-paths-demo/my-lib2';
+//
+// export const App = () => (
+//   <>
+//     <MyLib1 />
+//     <MyLib2 />
+//   </>
+// );
